84835a3a2f96b65d07a9c580b39c0743ef9ca7e1
oxo <miffi.oxo@gmail.com>
2023-02-01 00:40:15 +0900

    AssaultCubeGameHack 완성본
    다음 강의를 들은 뒤 만든 Assaultcube 게임핵입니다. 강의 내용에 더해, 몇 가지 기능(ex. 적 정보 조회 및 수정, 공속 높이기, etc)을 추가해보았습니다.
    
    [게임 해킹과 보안] FPS 오픈소스 게임핵 개발과 시큐어 코딩 강의 영상 링크
    https://www.youtube.com/watch?v=UUgGA24LbIQ&list=PLnIaYcDMsScxvz3yyClxLU9W6upAUyPzc
    
    [게임 해킹과 보안] FPS 오픈소스 게임핵 개발과 시큐어 코딩 깃허브 링크
    https://github.com/gasbugs/AssaultCubeGameHack

---
+++ AssaultCubeGameHack/AssaltCubeGameHack/Form1.cs
@@ -0,0 +1,372 @@
+﻿using ProcessMemoryReaderLib;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace AssaltCubeGameHack
+{
+    public partial class Form1 : Form
+    {
+        Process[] MyProcess;    // 프로세스를 목록을 저장할 장소
+        ProcessMemoryReader mem = new ProcessMemoryReader();
+        Process attachProc;
+        OverlayForm overlayForm = new OverlayForm();
+
+        bool attach = false;
+        bool healthHack = false;
+        bool ammoHack = false;
+        bool wallHack = false;
+        bool superjump = false;
+        bool attackHack = false;
+
+        PlayerData mainPlayer;
+        PlayerData[] enemyPlayer = new PlayerData[30];
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitBT_Click(object sender, EventArgs e)
+        {
+            DialogResult result;    // 정말 종료할 것인지 확인하는 변수
+            result = MessageBox.Show("종료하시겠습니까?", "종료메시지", MessageBoxButtons.OKCancel);
+
+            if (result == DialogResult.OK)
+            {
+                this.DialogResult = DialogResult.Abort;
+                Application.Exit();
+            }
+        }
+
+        // 클릭했을 때 프로세스 목록 나타나도록 하는 함수
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();    // 기존 프로세스 목록을 초기화
+            MyProcess = Process.GetProcesses(); // 프로세스 목록 불러오기
+
+            for (int i = 0; i < MyProcess.Length; i++)
+            {
+                string text = MyProcess[i].ProcessName + "-" + MyProcess[i].Id;
+                comboBox1.Items.Add(text);
+            }
+
+        }
+
+
+        // 콤보박스 메뉴중에 어떤 항목을 클릭했을 때 동작할 내용
+        // 프로세스를 선택했을 때 어떤 행동을 할지 정하기
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // 해당 프로세스에 대한 권한 가져오기
+                // 프로세스 메모리 읽기/수정
+                if (comboBox1.SelectedIndex != -1)  // 목록을 선택했다면
+                {
+                    string selectedItem = comboBox1.SelectedItem.ToString();    // ex. KakaoTalk-704
+                    int pid = int.Parse(selectedItem.Split('-')[selectedItem.Split('-').Length - 1]); // 문자열을 -로 나눈 후 가장 마지막 문자열으르 가져오기
+                    attachProc = Process.GetProcessById(pid);
+
+                    // 프로세스 열기 (권한)
+                    mem.ReadProcess = attachProc;
+                    mem.OpenProcess(); 
+
+                    MessageBox.Show("프로세스 열기 성공! " + attachProc.ProcessName);
+                    int base_ptr = attachProc.MainModule.BaseAddress.ToInt32() + 0x0055C424;
+                    int player_base = mem.ReadInt(base_ptr);
+                    mainPlayer = new PlayerData(player_base);
+
+                    attach = true;
+                }
+            }
+            catch (Exception ex)    // 예외 처리
+            {
+                attach = false;
+                MessageBox.Show("프로세스 열기 실패: " + ex.Message);
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)    // 0.001초마다 동작
+        {
+            if (attach)
+            {
+                try
+                {
+                    mainPlayer.SetPlayerData(mem);
+                    if (healthHack)
+                    {
+                        mainPlayer.hackHealth(mem);
+                    }
+
+                    if (ammoHack)
+                    {
+                        mainPlayer.hackAmmo(mem);
+                    }
+
+                    if (attackHack)
+                    {
+                        mainPlayer.hackAttack(mem);
+                    }
+
+                    // 마우스 오른쪽 키에 대한 상태를 확인
+                    int hotkey = ProcessMemoryReaderApi.GetKeyState(0x02);
+
+                    if (wallHack || (hotkey & 0x8000) != 0)
+                    {
+                        GetEnemyState(mem); // 적들에 대한 정보 습득
+                    }
+
+                    if (wallHack)
+                    {
+                        overlayForm.hackWall(mainPlayer, enemyPlayer);
+                    }
+
+                    
+                    if ((hotkey & 0x8000) != 0) // 키가 눌려있다면
+                    {
+                        float min_err = 100000; // 에러를 굉장히 큰 값으로 초기화
+                        float err = 0;
+                        double min_x_angle = 0;
+                        double min_y_angle = 0;
+                        
+
+                        for (int i = 0; i < 30; i++)
+                        {
+                            // aim hack algorithm
+                            
+                            err = mainPlayer.getAimErr(mem, enemyPlayer[i].head_x_angle, enemyPlayer[i].head_y_angle);
+                            if (min_err > err)
+                            {
+                                min_err = err;
+                                min_x_angle = enemyPlayer[i].head_x_angle;
+                                min_y_angle = enemyPlayer[i].head_y_angle;
+                            }
+                        }
+                        
+                        mainPlayer.hackAim(mem, min_x_angle, min_y_angle);
+                    }
+
+                    // 스페이스에 대한 상태 확인
+                    int hotkey2 = ProcessMemoryReaderApi.GetKeyState(0x51);
+                    if (superjump && (hotkey2 & 0x8000) != 0) // Q 키를 눌렀다면
+                    {
+                        mainPlayer.hackjump(mem);
+                    }
+
+                    // 데이터 모니터링
+                    HealthLBL.Text = "Health: " + mainPlayer.health;    // Health: 100
+                    AmmoLBL.Text = "Ammo: " + mainPlayer.ammo;
+                    BulletProofLBL.Text = "BulletProof: " + mainPlayer.bullet_proof;
+                    AngleLBL.Text = "Angle: " + mainPlayer.x_angle.ToString("#.##") + " | " + mainPlayer.y_angle.ToString("#.##");
+                    PositionLBL.Text = "Pos: " + mainPlayer.x_pos.ToString("#.##") + ", " + mainPlayer.y_pos.ToString("#.##") + "," + mainPlayer.z_pos.ToString("#.##");
+                    HighBL.Text = "High: " + mainPlayer.y_pos;
+
+                    try
+                    {
+                        if (comboBox2.SelectedIndex != -1)  // 목록을 선택했다면
+                        {
+                            int enemyNum = int.Parse(comboBox2.SelectedItem.ToString()); // 1 ~ 30
+
+                            GetEnemyState(mem);
+
+                            EHealthLBL.Text = "Health: " + enemyPlayer[enemyNum - 1].health;
+                            EAmmoLBL.Text = "Ammo: " + enemyPlayer[enemyNum - 1].ammo;
+                            EBulletProofLBL.Text = "BulletProof: " + enemyPlayer[enemyNum - 1].bullet_proof;
+                            EAngleLBL.Text = "Angle: " + enemyPlayer[enemyNum - 1].x_angle.ToString("#.##") + " | " + enemyPlayer[enemyNum - 1].y_angle.ToString("#.##");
+                            EPositionLBL.Text = "Pos: " + enemyPlayer[enemyNum - 1].x_pos.ToString("#.##") + ", " + enemyPlayer[enemyNum - 1].y_pos.ToString("#.##") + "," + enemyPlayer[enemyNum - 1].z_pos.ToString("#.##");
+                        }
+                    }
+                    catch (Exception ex) // 예외 처리
+                    {
+                        MessageBox.Show("적 정보 열람 실패: " + ex.Message);
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private double GetYDegree(PlayerData mainPlayer, PlayerData enemyPlayer)
+        {
+            double xz_distance = Math.Sqrt(Math.Pow(mainPlayer.x_pos - enemyPlayer.x_pos, 2) + Math.Pow(mainPlayer.z_pos - enemyPlayer.z_pos, 2));
+            double y = mainPlayer.y_pos - enemyPlayer.y_pos;
+            double correction = 1;
+
+            if (y > 0)
+            {
+                correction = -1;
+            }
+            return correction * Math.Abs( Math.Atan(y / xz_distance) * 180 / Math.PI);
+        }
+
+        private double Get2DDegree(PlayerData mainPlayer, PlayerData enemyPlayer)
+        {
+            double x = mainPlayer.x_pos - enemyPlayer.x_pos;
+            double z = mainPlayer.z_pos - enemyPlayer.z_pos;
+            double correction = 270;
+
+            if (x < 0) correction = 90;
+
+            return correction + Math.Atan(z / x) * 180 / Math.PI;
+        }
+
+        private double GetDistance(PlayerData mainPlayer, PlayerData enemyPlayer)
+        {
+            //피타고라스의 법칙을 사용해 xz_distance를 먼저 구하기 (2D)
+            double xz_distance = Math.Sqrt(Math.Pow(mainPlayer.x_pos- enemyPlayer.x_pos, 2) + Math.Pow(mainPlayer.z_pos - enemyPlayer.z_pos, 2));
+            //피타고라스의 법칙을 사용해 distance를 구하기 (3D)
+            double distance = Math.Sqrt(Math.Pow(xz_distance, 2) + Math.Pow(mainPlayer.y_pos - enemyPlayer.y_pos, 2));
+            return distance;
+        }
+
+        private void GetEnemyState(ProcessMemoryReader mem)
+        {
+            int base_ptr = attachProc.MainModule.BaseAddress.ToInt32() + 0x00583B08;
+
+            for (int i = 0; i < 30; i++)
+            {
+                int[] offsetArray = { i * 4, 0 }; // 0, 4, 8, 12 총 30명의 플레이어 데이터를 불러옴
+                int player_base = mem.ReadMultiLevelPointer(base_ptr, 4, offsetArray);
+                enemyPlayer[i] = new PlayerData(player_base);
+                enemyPlayer[i].SetPlayerData(mem);
+                enemyPlayer[i].distance = GetDistance(mainPlayer, enemyPlayer[i]);
+                enemyPlayer[i].head_x_angle = Get2DDegree(mainPlayer, enemyPlayer[i]);
+                enemyPlayer[i].head_y_angle = GetYDegree(mainPlayer, enemyPlayer[i]);
+            }
+
+            
+        }
+
+        private void HealthBT_Click(object sender, EventArgs e)
+        {
+            if (healthHack)
+            {
+                healthHack = false;
+                HealthHLBL.Text = "동작 안함";
+            }
+            else
+            {
+                healthHack = true;
+                HealthHLBL.Text = "동작 중";
+
+            }
+        }
+
+        private void AmmoBT_Click(object sender, EventArgs e)
+        {
+            if (ammoHack)
+            {
+                ammoHack = false;
+                ammoHLBL.Text = "동작 안함";
+            }
+            else
+            {
+                ammoHack = true;
+                ammoHLBL.Text = "동작 중";
+            }
+        }
+
+        private void WallHackCHB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (WallHackCHB.Checked == true) // 만약에 체크박스가 체킹되어 있다면
+            {
+                overlayForm.Show();
+                wallHack = true;
+            }
+            else // 만약에 체크박스가 체킹돼있지 않다면
+            {
+                overlayForm.Hide();
+                wallHack = false;
+            }
+        }
+
+        // 적의 정보 조작하기
+        private void changeEnemyStateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox2.SelectedIndex != -1) // 목록을 선택했다면
+                {
+                    int enemyNum = int.Parse(comboBox2.SelectedItem.ToString()); // 1 ~ 30
+                    
+                    GetEnemyState(mem);
+
+                    // health 변경사항 있다면 변경
+                    if (changeEnemyHealth.Text != "")
+                        enemyPlayer[enemyNum - 1].changeEnemyHealth(mem, Convert.ToInt32(changeEnemyHealth.Text));
+
+                    // ammo 변경사항 있다면 변경
+                    if (changeEnemyAmmo.Text != "")
+                        enemyPlayer[enemyNum - 1].changeEnemyAmmo(mem, Convert.ToInt32(changeEnemyAmmo.Text));
+
+                    // bullet proof 변경사항 있다면 변경
+                    if (changeEnemyBulletProof.Text != "")
+                        enemyPlayer[enemyNum - 1].changeEnemyBulletProof(mem, Convert.ToInt32(changeEnemyBulletProof.Text));
+                }
+            }
+            catch (Exception ex) // 예외 처리
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AttackBT_Click(object sender, EventArgs e)
+        {
+            if (attackHack)
+            {
+                attackHack = false;
+                AttackHLBL.Text = "동작 안함";
+            }
+            else
+            {
+                attackHack = true;
+                AttackHLBL.Text = "동작 중";
+
+            }
+        }
+
+        // 슈퍼점프 해제하고 원 상태로 돌아오도록 하기
+        private void ResetBT_Click(object sender, EventArgs e)
+        {
+            mainPlayer.reset(mem);
+        }
+
+        // 슈퍼점프 활성화
+        private void JumpCHB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (JumpCHB.Checked == true)
+            {
+                superjump = true;
+            }
+            else
+            {
+                superjump = false;
+            }
+        }
+
+        // 플레이어를 구급상자로 이동시키기
+        private void MoveBT_Click(object sender, EventArgs e)
+        {
+            mainPlayer.hackheal(mem);
+        }
+
+        // 플레이어 정보 조작하기
+        private void changePlayerStateBtn_Click(object sender, EventArgs e)
+        { 
+            // health 변경사항 있다면 변경
+            if (changePlayerHealth.Text != "")
+                mainPlayer.changeEnemyHealth(mem, Convert.ToInt32(changePlayerHealth.Text));
+
+            // ammo 변경사항 있다면 변경
+            if (changePlayerAmmo.Text != "")
+                mainPlayer.changeEnemyAmmo(mem, Convert.ToInt32(changePlayerAmmo.Text));
+
+            // bullet proof 변경사항 있다면 변경
+            if (changePlayerBulletProof.Text != "")
+                mainPlayer.changeEnemyBulletProof(mem, Convert.ToInt32(changePlayerBulletProof.Text));
+        }
+    }
+}
---
+++ AssaultCubeGameHack/AssaltCubeGameHack/PlayerData.cs
@@ -0,0 +1,139 @@
+﻿using ProcessMemoryReaderLib;
+using System;
+using System.Diagnostics;
+
+namespace AssaltCubeGameHack
+{
+    internal class PlayerData
+    {
+        int base_addr;// "ac_client.exe"+0x0055C424 구조체의 위치
+
+        // 구조체 모양
+        int health_offset = 0xEC;
+        int bullet_proof_offset = 0xF0;
+        int ammo_offset = 0x140;
+        int x_pos_offset = 0x28;
+        int z_pos_offset = 0x2C;
+        int y_pos_offset = 0x30;
+        int x_angle_offset = 0x34;
+        int y_angle_offset = 0x38;
+        int weapon1_offset = 0x164; // 1번 총
+        int weapon2_offset = 0x150; // 2번 총
+        int weapon4_offset = 0x14C; // 4번 칼
+
+        // 캐릭터 정보
+        public int health;
+        public int bullet_proof;
+        public int ammo;
+        public float x_pos;
+        public float y_pos; 
+        public float z_pos;
+        public float x_angle;
+        public float y_angle;
+        public double distance;
+        public double head_x_angle;
+        public double head_y_angle;
+
+        public PlayerData(int player_base)
+        {
+            // mainPlayer 또는 적 플레이어 구조체의 위치
+            base_addr = player_base;
+            health = 0;
+            bullet_proof = 0;
+            ammo = 0;
+            x_pos = 0;
+            y_pos = 0;
+            z_pos = 0;
+            x_angle = 0;
+            y_angle = 0;
+            distance = 0;
+            head_x_angle = 0;
+            head_y_angle = 0;
+        }
+
+        public void SetPlayerData(ProcessMemoryReader mem)
+        {
+            health = mem.ReadInt(base_addr + health_offset);
+            bullet_proof = mem.ReadInt(base_addr + bullet_proof_offset);
+            ammo = mem.ReadInt(base_addr + ammo_offset);
+            x_pos = mem.ReadFloat(base_addr + x_pos_offset);
+            y_pos = mem.ReadFloat(base_addr + y_pos_offset);
+            z_pos = mem.ReadFloat(base_addr + z_pos_offset);
+            x_angle = mem.ReadFloat(base_addr + x_angle_offset);
+            y_angle = mem.ReadFloat(base_addr + y_angle_offset);
+        }
+
+        internal void hackHealth(ProcessMemoryReader mem)
+        {
+            mem.WriteInt(base_addr + health_offset, 1000); // 체력 1000으로
+        }
+
+        internal void hackAmmo(ProcessMemoryReader mem)
+        {
+            mem.WriteInt(base_addr + ammo_offset, 1000); // 탄약 1000으로
+        }
+
+        internal void hackjump(ProcessMemoryReader mem) // 슈퍼점프
+        {
+            mem.WriteFloat(base_addr + y_pos_offset, 10.0f); // 기존 높이에서 10만큼 점프
+        }
+
+        internal void reset(ProcessMemoryReader mem)    // 슈퍼점프 해제하고 원 상태로 돌아오도록 하기
+        {
+            mem.WriteFloat(base_addr + y_pos_offset, 2.0f);
+        }
+
+        internal void hackheal(ProcessMemoryReader mem) // 구급상자의 위치로 이동
+        {
+            mem.WriteFloat(base_addr + x_pos_offset, 77.39f);
+            mem.WriteFloat(base_addr + y_pos_offset, 1.0f);
+            mem.WriteFloat(base_addr + z_pos_offset, 76.93f);
+        }
+
+        internal void hackAim(ProcessMemoryReader mem, double x_angle, double y_angle)
+        {
+            float _x = Double2Float(x_angle);
+            float _y = Double2Float(y_angle);
+            mem.WriteFloat(base_addr + x_angle_offset, _x); // x 각도 세팅
+            mem.WriteFloat(base_addr + y_angle_offset, _y); // y 각도 세팅
+        }
+        internal void hackAttack(ProcessMemoryReader mem)
+        {
+            mem.WriteInt(base_addr + weapon1_offset, 30); // 1번 총 공격속도 상승
+            mem.WriteInt(base_addr + weapon2_offset, 30); // 2번 총 공격속도 상승
+            mem.WriteInt(base_addr + weapon4_offset, 30); // 4번 칼 공격속도 상승
+        }
+
+        private float Double2Float(double input)
+        {
+            float result = (float)input;
+            if (float.IsPositiveInfinity(result))
+            {
+                result = float.MaxValue;
+            }
+            else if (float.IsNegativeInfinity(result))
+            {
+                result = float.MinValue;
+            }
+            return result;
+        }
+
+        internal float getAimErr(ProcessMemoryReader mem, double _x_angle, double _y_angle)
+        {
+            return Double2Float(Math.Pow(x_angle - _x_angle, 2) + Math.Pow(y_angle - _y_angle, 2));
+        }
+
+        internal void changeEnemyHealth(ProcessMemoryReader mem, int health)
+        {
+            mem.WriteInt(base_addr + health_offset, health);
+        }
+        internal void changeEnemyAmmo(ProcessMemoryReader mem, int ammo)
+        {
+            mem.WriteInt(base_addr + ammo_offset, ammo);
+        }
+        internal void changeEnemyBulletProof(ProcessMemoryReader mem, int bullet_proof)
+        {
+            mem.WriteInt(base_addr + bullet_proof_offset, bullet_proof);
+        }
+    }
+}
